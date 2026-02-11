487ae79109b948b58adbe006caf90da19b8652d3
Mohammad Saif Al Islam <saif404.ist@gmail.com>
2026-02-11 10:31:57 +0600

    Fix isPrime function to return true for number 2

--- 1basic.js
+++ 1basic.js
@@ -126,7 +126,7 @@ out(`The number of vowels in text ${text2} is ${count}`);
 function isPrime(num) {
 
     if(num <= 1) return false;
-    if(num == 2) return false;
+    if(num == 2) return true;
     if(num%2 === 0) return false;
 
     for(let i = 3 ; i<Math.sqrt(num); i+=2) {
